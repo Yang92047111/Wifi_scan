f7e4e89c459f1f5ce8f457e31ab340b3599ca008
Yang <okm92047111@gmail.com>
2020-03-05 13:52:57 +0800

    Fix LabelComponent.kt

--- app/src/main/java/com/takehara/tsutou/w_ifiscanner/Fragment/LabelComponent.kt
+++ app/src/main/java/com/takehara/tsutou/w_ifiscanner/Fragment/LabelComponent.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
 import androidx.fragment.app.Fragment
 import com.takehara.tsutou.w_ifiscanner.R
 import kotlinx.android.synthetic.main.fragment_label_component.view.*
