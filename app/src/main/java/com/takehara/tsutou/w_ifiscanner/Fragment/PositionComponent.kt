55fd138c162077dc2b41194ba78bd94611b28b85
James <james.lee0722@gmail.com>
2020-03-09 12:29:14 +0800

    Position fragment keep scanning wifi and posting

--- app/src/main/java/com/takehara/tsutou/w_ifiscanner/Fragment/PositionComponent.kt
+++ app/src/main/java/com/takehara/tsutou/w_ifiscanner/Fragment/PositionComponent.kt
@@ -24,6 +24,7 @@ import javax.net.ssl.TrustManager
 import javax.net.ssl.X509TrustManager
 import android.content.Intent
 import android.graphics.Color
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
@@ -31,6 +32,14 @@ import kotlinx.android.synthetic.main.fragment_position_component.*
 import kotlinx.android.synthetic.main.fragment_position_component.view.*
 import org.angmarch.views.NiceSpinner
 
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [PositionComponent.newInstance] factory method to
+ * create an instance of this fragment.
+ */
 class PositionComponent : Fragment() {
     // TODO: Rename and change types of parameters
 
@@ -38,11 +47,12 @@ class PositionComponent : Fragment() {
     private var beforewifidata: ArrayList<List<Data>>? = ArrayList()
     private val gson = Gson()
     private var wifiReceiverRegistered: Boolean = false
-    private var jsonString: ArrayList<List<Data>>? = ArrayList()
+    private var jsonString: ArrayList<List<PositionComponent.Data>>? = ArrayList()
+    private var taskHandler = Handler()
 
     data class Upload(
         @SerializedName("test") var test: Boolean,
-        @SerializedName("data") var data: ArrayList<List<Data>>
+        @SerializedName("data") var data: ArrayList<List<PositionComponent.Data>>
     )
     data class Data (
         @SerializedName("mac") var mac: String,
@@ -55,25 +65,24 @@ class PositionComponent : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_position_component, container, false)
         val car_types = mutableListOf<String>("N95", "N96")
-        val car_spinner = view.car_spinner as NiceSpinner
-        car_spinner.setTextColor(Color.BLACK)
-        car_spinner.attachDataSource(car_types)
+
         view.classroom.setText("綜合科館109-2")
         return view
     }
 
     private fun wifiList() {
-        val APdata = ArrayList<Data>()
+        val APdata = ArrayList<PositionComponent.Data>()
         val results = wifiManager.scanResults as ArrayList<ScanResult>
         for (result in results) {
             val mac = result.BSSID.replace(":", "")
             val newData =
-                Data(mac = mac, rssi = result.level.toString(), ssid = result.SSID)
+                PositionComponent.Data(mac = mac, rssi = result.level.toString(), ssid = result.SSID)
             APdata.add(newData)
         }
 
         jsonString = arrayListOf(APdata)
         wifidata= jsonString
+        Log.i("wifitest",jsonString.toString())
     }
 
     companion object {
@@ -88,12 +97,10 @@ class PositionComponent : Fragment() {
             val results = wifiManager.scanResults as ArrayList<ScanResult>
         }
     }
-
     private fun UploadAPI() {
-
         val data =
             jsonString?.let {
-                Upload(
+                PositionComponent.Upload(
                     test = true,
                     data = it
                 )
@@ -154,6 +161,7 @@ class PositionComponent : Fragment() {
             override fun onResponse(call: Call, response: Response
             )  {
                 val room =response.body!!.string()
+                Log.d("STATUS", room)
                 Thread(Runnable {
                     activity?.runOnUiThread(java.lang.Runnable {
                         classroom.setText(room)
@@ -162,31 +170,38 @@ class PositionComponent : Fragment() {
             }
         })
     }
+    private var runnable = object:Runnable {
+        override fun run() {
+            beforewifidata = wifidata
+            wifiManager.startScan()
+            wifiList()
+            Log.i("gg", "ggg")
+            if (beforewifidata != wifidata) {
+                beforewifidata = wifidata
+                UploadAPI()
+            }
+            taskHandler.postDelayed(this,5000)
+        }
+    }
 
     override fun onResume() {
         super.onResume()
+        wifiManager.startScan()
         wifiList()
         UploadAPI()
-        beforewifidata = wifidata
-
-        classroom.setOnClickListener(){
-            wifiList()
-            if(beforewifidata!=wifidata) {
-
-                beforewifidata=wifidata
-                UploadAPI()
-            }
-        }
+        taskHandler.postDelayed(runnable,5000)
     }
 
     override fun onStop() {
         stopScanning()
         super.onStop()
+        taskHandler.removeCallbacksAndMessages(null)
     }
 
     private fun startScanning() {
         if (checkPermissions()) {
             activity?.applicationContext?.registerReceiver(wifiReceiver, IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
+            Log.i("","scan")
             wifiReceiverRegistered = true
         }
     }
@@ -207,6 +222,7 @@ class PositionComponent : Fragment() {
                 arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
                 PERMISSION_REQUEST_CODE_ACCESS_COARSE_LOCATION
             )
+
             return false
         } else {
             return true
@@ -218,6 +234,7 @@ class PositionComponent : Fragment() {
         when (requestCode) {
             PERMISSION_REQUEST_CODE_ACCESS_COARSE_LOCATION -> {
                 startScanning()
+
             }
         }
     }
